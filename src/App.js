0d882f859f456ccdfe307f2e5518cda50b1124e6
hjzheng <zhenghaoju700@163.com>
2019-05-05 17:22:38 +0800

    dnd example

--- src/App.js
+++ src/App.js
@@ -1,87 +1,101 @@
 import React from 'react';
-import marked from 'marked';
-import 'github-markdown-css';
+import { DragDropContext } from 'react-beautiful-dnd'
 import './App.scss';
-
-const IconButton = ({icon, onClick}) => {
-  return (
-    <i className={`iconfont ${icon}`} onClick={onClick} />
-  );
-}
-
+import initialData from './initialData';
+import Column from './Column';
 
 class App extends React.Component {
 
-  textareaRef = React.createRef();
+  state = initialData;
 
-  state = {
-    markdown: ''
-  }
+  onDragEnd = result => {
+    
+    // reorder our column
 
-  onChangeHandler = (e) => {
-    const { value } = e.target;
-    this.setState({
-      markdown: value
-    });
+    console.log(result);
+    const { destination, source, draggableId } = result;
 
-    localStorage.setItem('markdownValue', value);
-  }
-
-  makeBolder = () => {
-    const {selectionStart, selectionEnd} = this.textareaRef.current;
-    const markdown = this.state.markdown;
-    let newMarkdown = markdown.substring(0, selectionStart) + 
-                      '**' +
-                      markdown.substring(selectionStart, selectionEnd) +
-                      '**' +
-                      markdown.substring(selectionEnd);
-
-    this.setState({markdown: newMarkdown});                  
-  }
-
-  makeItalic = () => {
-
-  }
-
-  makeCode = () => {
-
-  }
-
-  componentDidMount() {
-    const markdown = localStorage.getItem('markdownValue');
-    if (markdown) {
-      this.setState({markdown});
+    // no destination
+    if (!destination) {
+      return;
     }
-  }
+
+    // no move
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const column = this.state.columns[source.droppableId];
+
+    const newTaskIds = Array.from(column.taskIds);
+
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds
+    };
+
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,     
+         [newColumn.id]: newColumn,
+      },
+    };
+
+    this.setState(newState);
+
+  };
 
   render() {
-    const { markdown } = this.state;
-    const html = marked(markdown);
+
+    const { columnOrder, columns, tasks } = this.state;
 
     return (
-      <div className="App">
-        <div className="markdown">
-          <div className="toolbar">
-            <IconButton icon={'icon-bold'} onClick={this.makeBolder} />
-            <IconButton icon={'icon-italic'} onClick={this.makeItalic} />
-            <IconButton icon={'icon-code'} onClick={this.makeCode} />
-          </div>
-          <div className="body">
-            <div className="editor">
-              <textarea className="textarea" 
-                value={this.state.markdown}
-                onChange={(e) => {
-                  this.onChangeHandler(e);
-                }}
-                ref={this.textareaRef} />
-            </div>
-            <div className="preview markdown-body" dangerouslySetInnerHTML={{__html: html}}>
-            </div>
-          </div>
-        </div>
-      </div>
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        {
+          columnOrder.map(columnId => {
+            const _column = columns[columnId];
+            const _tasks = _column.taskIds.map(taskId => tasks[taskId]);
+
+            return <Column key={_column.id} column={_column} tasks={_tasks}/>
+          })
+        }
+      </DragDropContext>
     );
   }
 }
 
 export default App;
+
+/*
+// onDragStart
+const start = {
+    draggableId: 'task-1',
+    type: 'TYPE'
+    source: {
+        droppableId: 'column-1',
+        index: 0,
+    }
+}
+
+//onDragUpdate
+const update= {
+    ...start,
+    destination: {
+        droppableId: 'column-1',
+        index: 1,
+    }
+};
+
+// onDragEnd
+const result - {
+    ...update,
+    reason: 'DROP',
+}
+*/
---
+++ src/Column.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Droppable } from 'react-beautiful-dnd';
+import Task from './Task';
+
+const Container = styled.div`
+    margin: 8px;
+    border: 1px solid lightgrey;
+    border-radius: 2px;
+`;
+const Title = styled.h2`
+    padding: 8px;
+`;
+const TaskList = styled.div`
+    padding: 8px;
+    background-color: ${props => (props.isDraggingOver ? "skyblue" : "white")};
+`;
+
+class Column extends React.Component {
+    render() {
+
+        const { column, tasks } = this.props;
+
+        return (
+            <Container>
+                <Title>{column.title}</Title>
+                <Droppable droppableId={column.id}>
+                    {(provided, snapshot) => (
+                        <TaskList ref={provided.innerRef} 
+                            {...provided.droppableProps}
+                            isDraggingOver={snapshot.isDraggingOver}
+                        >
+                            {tasks.map((task, index) => <Task key={task.id} task={task} index={index} />)}
+                            {provided.placeholder}
+                        </TaskList>
+                    )}
+                </Droppable>
+            </Container>
+        )
+    }
+}
+
+export default Column;
+
+/*
+//Droppable
+const droppableSnapshot = {
+  isDraggingOver: true,
+  draggingOverWith: "task-1"
+};
+*/
