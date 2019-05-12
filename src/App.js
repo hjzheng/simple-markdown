b44d56daecd7bb95604ded8e1635b204c85549de
hjzheng <zhenghaoju700@163.com>
2019-05-12 20:29:59 +0800

    table test

--- src/App.js
+++ src/App.js
@@ -9,42 +9,146 @@ const dataList = [
   { id: 3, name: 'c', email: 'c@email.com', avartar: '...' }
 ];
 
+class EditCell extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      editing: false,
+      value: ''
+    }
+  }
+
+  setEditing = (e) => {
+
+    e.stopPropagation();
+
+    const { rowData, dataKey } = this.props;
+    this.setState(
+      {
+        editing: true,
+        value: rowData[dataKey]
+      }
+    );
+  }
+
+  save = (e) => {
+    e.stopPropagation();
+
+    const { rowData, dataKey } = this.props;
+
+    this.props.onValueChange(this.state.value, rowData, dataKey);
+
+    this.setState({editing: false});
+  }
+
+  cancel = (e) => {
+    e.stopPropagation();
+
+    this.setState({editing: false});
+  }
+
+  onChange = (e) => {
+    this.setState({value: e.target.value});
+  }
+
+  render() {
+    
+    const { rowData, dataKey, onValueChange, ...props} = this.props;
+
+    return (
+      <Cell {...props} onClick={this.setEditing}>
+        { 
+          this.state.editing === true ? 
+          <div>
+            <input 
+              ref={this.inputRef} 
+              value={this.state.value} 
+              onChange={this.onChange} />
+              <button onClick={this.save}>Save</button>
+              <button onClick={this.cancel}>Cancel</button>
+          </div> : 
+          rowData[dataKey]
+        }
+      </Cell>
+    )
+  }
+}
+
 const ImageCell = ({ rowData, dataKey, ...props }) => (
   <Cell {...props}>
-    <img src={rowData[dataKey]} width="50" />
+    <img src={rowData[dataKey]} alt="Avartar" width="50" />
   </Cell>
 );
 
-
 class App extends React.Component {
 
+  state = {
+    dataList: dataList,
+    columns: [
+      {
+        label: 'ID',
+        dataKey: 'id'
+      },
+      {
+        label: 'Name',
+        dataKey: 'name'
+      },
+      {
+        label: 'Email',
+        dataKey: 'email'
+      },
+      {
+        label: 'Avartar',
+        dataKey: 'avartar'
+      }
+    ]
+  }
+
+  onChange = (newValue, rowData, dataKey) => {
+
+    const dataList = this.state.dataList.map(data => {
+      if(data.id === rowData.id) {
+        data[dataKey] = newValue
+      }
+      return data;
+    })
+    this.setState({dataList});
+  }
+
+  renderCell(dataKey) {
+    switch (dataKey) {
+        case 'name':
+          return <EditCell dataKey={dataKey} onValueChange={this.onChange}/>
+        case 'avartar':
+          return <ImageCell dataKey={dataKey} />
+        default:
+          return <Cell dataKey={dataKey} />
+    }
+  }
+
+  renderColumn(label, dataKey) {
+    return (
+      <Column width={300} sort fixed resizable>
+        <HeaderCell>{label}</HeaderCell>
+        {
+          this.renderCell(dataKey)
+        }
+      </Column>
+    );
+  }
   
   render() {
+
+    const { dataList, columns } = this.state;
   
     return (
       <div className="App">
-        React 学习练习模板代码
         <Table data={dataList}>
-          <Column width={100} sort fixed resizable>
-            <HeaderCell>ID</HeaderCell>
-            <Cell dataKey="id" />
-          </Column>
-
-          <Column width={100} sort resizable>
-            <HeaderCell>Name</HeaderCell>
-            <Cell dataKey="name" />
-          </Column>
-
-          <Column width={100} sort resizable>
-            <HeaderCell>Email</HeaderCell>
-            <Cell dataKey="email" />
-          </Column>
-
-          <Column width={100} resizable>
-            <HeaderCell>Avartar</HeaderCell>
-            <ImageCell dataKey="avartar" />
-          </Column>
-        </Table>;
+          {
+            columns.map(({label, dataKey}) => this.renderColumn(label, dataKey))
+          }
+        </Table>
       </div>
     );
   }
