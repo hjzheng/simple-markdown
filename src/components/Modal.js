a8437cefc9c39e8384f1aba86a83e165fab7eaa1
hjzheng <zhenghaoju700@163.com>
2019-05-17 00:40:50 +0800

    use Hooks for Modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,154 +1,128 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';  
 import cx from 'classnames';
 import './Modal.scss';
 
-class Modal extends React.Component {
+const Modal = ({
+    title,
+    className,
+    children,
+    visible = false, 
+    buttons =  [
+        {text: 'OK', isPrimary: true, type: 'Ok'},
+        {text: 'Cancel', type: 'Cancel'}
+    ],
+    onRequestClose, 
+    onOk = () => {},
+    onCancel = () => {}
+}) => {
 
-    static propTypes = {
-        visible: PropTypes.bool,
-        buttons: PropTypes.arrayOf(PropTypes.shape(
-            {
-                text: PropTypes.string.isRequired,
-                type: PropTypes.string,
-                isPrimary: PropTypes.bool
-            }
-        )),
-        onRequestClose: PropTypes.func.isRequired,
-        onOk: PropTypes.func,
-        onCancel: PropTypes.func,
+    const onButtonClick = (type) => {
+        type === 'Ok' ? onOk() : onCancel();
     }
 
-    static defaultProps = {
-        visible: false,
-        buttons: [
-            {text: 'OK', isPrimary: true, type: 'Ok'},
-            {text: 'Cancel', type: 'Cancel'}
-        ],
-        onOk: () => {},
-        onCancel: () => {}
-    }
-
-    componentDidMount() {
+    useEffect(() => {
 
         const keydownhandler = e => {
             if (e.keyCode === 27) {
-                this.props.onRequestClose();
+                onRequestClose();
             }
         }
 
         window.addEventListener('keydown', keydownhandler);
-
-        this.clear = () => {
+        
+        return () => {
             window.removeEventListener('keydown', keydownhandler);
         }
-    }
+    })
 
-    componentWillUnmount() {
-        this.clear();
-    }
+    const mask = ReactDOM.createPortal(<div className="modal-backdrop fade show" />, document.body)
 
-    onButtonClick = (type) => {
-        this.props[`on${type}`]();
-    }
-
-    render() {
-
-        const { title, className, visible, children, buttons, onRequestClose } = this.props;
-
-        const mask = ReactDOM.createPortal(<div className="modal-backdrop fade show" />, document.body)
-
-        return (
-            <ReactCSSTransitionGroup
-                transitionName="example"
-                transitionEnterTimeout={1000}
-                transitionLeaveTimeout={300}>
-                { visible ? 
-                <div className={className}>
-                    <div className="modal" tabIndex="-1" role="dialog" style={{display: 'block'}}>
-                        <div className="modal-dialog" role="document">
-                            <div className="modal-content">
-                            <div className="modal-header">
-                                <h5 className="modal-title">{ title }</h5>
-                                <button onClick={onRequestClose} type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div className="modal-body">
-                                { children }
-                            </div>
-                            <div className="modal-footer">
-                                {
-                                    buttons.map((btn, index) => {
-                                        return ( 
-                                            <button 
-                                                key={index} 
-                                                type="button" 
-                                                className={cx("btn", btn.isPrimary ? 'btn-primary' : 'btn-secondary')} 
-                                                onClick={() => this.onButtonClick(btn.type)}
-                                                data-dismiss="modal">
-                                                    {btn.text}
-                                            </button>)
-                                    })
-                                }
-                            </div>
-                            </div>
+    return (
+        <ReactCSSTransitionGroup
+            transitionName="example"
+            transitionEnterTimeout={1000}
+            transitionLeaveTimeout={300}>
+            { visible ? 
+            <div className={className}>
+                <div className="modal" tabIndex="-1" role="dialog" style={{display: 'block'}}>
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">{ title }</h5>
+                            <button onClick={onRequestClose} type="button" className="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            { children }
+                        </div>
+                        <div className="modal-footer">
+                            {
+                                buttons.map((btn, index) => {
+                                    return ( 
+                                        <button 
+                                            key={index} 
+                                            type="button" 
+                                            className={cx("btn", btn.isPrimary ? 'btn-primary' : 'btn-secondary')} 
+                                            onClick={() => onButtonClick(btn.type)}
+                                            data-dismiss="modal">
+                                                {btn.text}
+                                        </button>)
+                                })
+                            }
+                        </div>
                         </div>
                     </div>
-                    {mask}
-                </div> : 
-                null
-                }
-            </ReactCSSTransitionGroup>
-        );
-    }
+                </div>
+                {mask}
+            </div> : 
+            null
+            }
+        </ReactCSSTransitionGroup>
+    );
 }
 
+const ModalWithState = ({
+    title, 
+    children, 
+    onOk = () => {}, 
+    onCancel = () => {}, 
+    onRequestClose = () => {}
+}) => {
+    const [visible, setVisible] = useState(false);
 
-class ModalWithState extends React.Component {
+    useEffect(() => {
+        setVisible(true);
+    }, [visible])
 
-    state = {
-        visible: false
+    const requestClose = () => {
+        onRequestClose && onRequestClose();
+        setVisible(false);
     }
-
-    componentDidMount() {
-        this.setState({ visible: true });
+        
+    const handleOk = () => {
+        onOk();
+        requestClose();
     }
-
-    onRequestClose = () => {
-        this.props.onRequestClose && this.props.onRequestClose();
-        this.setState({visible: false});
-    }
-
-    handleOk = () => {
-        this.props.onOk && this.props.onOk();
+        
+    const handleCancel = () => {
+        onCancel();
         this.onRequestClose();
     }
 
-    handleCancel = () => {
-        this.props.onCancel && this.props.onCancel();
-        this.onRequestClose();
-    }
-
-    render() {
-
-        const { visible } = this.state;
-        const { title, children } = this.props;
-
-        return (
-            <Modal
-                title={title}
-                visible={visible}
-                onRequestClose={this.onRequestClose}
-                onOk={this.handleOk}
-                onCancel={this.handleCancel}
-            >
-                { children }
-            </Modal> 
-        )
-    }
+    return (
+        <Modal
+            title={title}
+            visible={visible}
+            onRequestClose={requestClose}
+            onOk={handleOk}
+            onCancel={handleCancel}
+        >
+            { children }
+        </Modal> 
+    );
 }
 
 const confirmDOM = document.createElement('div');
